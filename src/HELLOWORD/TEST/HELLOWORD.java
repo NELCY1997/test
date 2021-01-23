0b6c5858a18856c55c3dc3527699c09368c16282
nelcy <nelcy@DESKTOP-5U4JMOF>
2021-01-23 11:30:31 -0600

    VERSION 1.2

--- src/HELLOWORD/TEST/HELLOWORD.java
+++ src/HELLOWORD/TEST/HELLOWORD.java
@@ -12,7 +12,7 @@ package HELLOWORD.TEST;
 public class HELLOWORD {
     
     public static void main (String[] args) {
-        System.out.println("Hola :D");
+        System.out.println("UNAM");
     }
         
 }
